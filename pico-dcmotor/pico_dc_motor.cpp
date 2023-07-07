f60f3f3a6c174dce6a04ad2959e553f99d5dc355
Lukkelele <encryptedluke@protonmail.com>
2023-07-07 14:20:09 +0200

    PWM code

--- pico-dcmotor/pico_dc_motor.cpp
+++ pico-dcmotor/pico_dc_motor.cpp
@@ -1,6 +1,5 @@
 #include "pico_dc_motor.h"
 
-constexpr int sleep_time = 2000;
 
 DCMotor::DCMotor()
 {
@@ -8,31 +7,29 @@ DCMotor::DCMotor()
 
 void DCMotor::Init()
 {
-    gpio_init(FORWARD_PIN);
-    gpio_set_dir(FORWARD_PIN, GPIO_OUT);
-
-    // Pico onboard LED
     gpio_init(ONBOARD_LED);
+    gpio_init(PIN_15);
     gpio_set_dir(ONBOARD_LED, GPIO_OUT);
-    // gpio_put(ONBOARD_LED, 1);
+    gpio_set_dir(PIN_15, GPIO_OUT);
+
+    gpio_put(ONBOARD_LED, 1);
+    // Light green LED before initializing PWM
+    gpio_put(PIN_15, 1); 
+    InitPWM(PWM_PIN, PWM_RANGE);
+    gpio_put(PIN_15, 0); 
 }
 
 void DCMotor::OnUpdate()
 {
-    if (IsButtonPressed(FORWARD_BUTTON))
-    {
-        Forward(MAX_POWER);
-    }
-
-    else
-    {
-        Neutral();
-    }
 }
 
-void DCMotor::SetLED(uint pin, int val)
+void DCMotor::InitPWM(uint pin, float pwm_range)
 {
-    gpio_put(pin, val);
+    gpio_set_function(pin, GPIO_FUNC_PWM);
+    m_SliceNum = pwm_gpio_to_slice_num(pin);
+    pwm_set_wrap(m_SliceNum, pwm_range);
+    pwm_set_clkdiv(m_SliceNum, 2.0f);
+    pwm_set_enabled(m_SliceNum, true);
 }
 
 void DCMotor::ToggleLED(uint pin)
@@ -48,21 +45,35 @@ void DCMotor::ToggleLED(uint pin)
 
 void DCMotor::Forward(float p)
 {
-    if (m_Forward == false)
-    {
-        m_Forward = true;
-        gpio_put(FORWARD_PIN, 1);
-    }
+    // TODO: Checker for same value - skip if same
+    m_Forward = true;
+    pwm_set_chan_level(m_SliceNum, PWM_CHAN_A, p);
 }
 
 void DCMotor::Backward(float p)
 {
+    pwm_set_chan_level(m_SliceNum, PWM_CHAN_A, -p);
 }
 
 void DCMotor::Neutral()
 {
-    gpio_put(FORWARD_PIN, 0);
     m_Forward = false;
+    pwm_set_chan_level(m_SliceNum, PWM_CHAN_A, 0);
+}
+
+void DCMotor::UpdateDutyCycle(bool forward)
+{
+    if (forward)
+    {
+        m_DutyCycle++;
+        return;
+    }
+    m_DutyCycle--;
+}
+
+void DCMotor::SetDutyCycle(int cycle)
+{
+    m_DutyCycle = cycle;
 }
 
 bool DCMotor::IsButtonPressed(uint button)
@@ -73,29 +84,40 @@ bool DCMotor::IsButtonPressed(uint button)
 
 int main()
 {
-    // setup_default_uart();
     stdio_init_all();
+    
     printf("Hello World\n");
 
-    const uint LED_PIN = 25;
-    const uint PIN_15 = 15;
-    gpio_init(LED_PIN);
-    gpio_init(PIN_15);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
-    gpio_set_dir(PIN_15, GPIO_OUT);
-    gpio_put(LED_PIN, 1);
-    gpio_put(PIN_15, 1);
+
+    DCMotor dcMotor;
+    dcMotor.Init();
 
     while (true)
     {
+        gpio_put(PIN_15, 1); // Green LED
+        for (uint dutyCycle = 0; dutyCycle <= PWM_RANGE; dutyCycle++)
+        {
+            dcMotor.SetDutyCycle(dutyCycle);
+            pwm_set_chan_level(dcMotor.GetDutyCycle(), PWM_CHAN_A, dutyCycle);
+            sleep_ms(10);
+        }
+        gpio_put(PIN_15, 0);
+
+        for (uint dutyCycle = PWM_RANGE; dutyCycle > 0; dutyCycle--)
+        {
+            dcMotor.SetDutyCycle(dutyCycle);
+            pwm_set_chan_level(dcMotor.GetDutyCycle(), PWM_CHAN_A, dutyCycle);
+            sleep_ms(10);
+        }
+
         // LED ON
-        gpio_put(LED_PIN, 1);
+        gpio_put(ONBOARD_LED, 1);
         gpio_put(PIN_15, 1);
         printf("LED ON\n");
         sleep_ms(sleep_time);
 
         // LED OFF
-        gpio_put(LED_PIN, 0);
+        gpio_put(ONBOARD_LED, 0);
         gpio_put(PIN_15, 0);
         printf("LED OFF\n");
         sleep_ms(sleep_time);
--- pico-dcmotor/pico_dc_motor.h
+++ pico-dcmotor/pico_dc_motor.h
@@ -4,12 +4,19 @@
 #include <unistd.h>
 #include <pico/stdlib.h>
 #include <hardware/gpio.h>
+#include <hardware/pwm.h>
 
-#define FORWARD_PIN 15   // GPIO 15
-#define FORWARD_BUTTON 6 // GPIO 6
-#define ONBOARD_LED 25   // GPIO 25
+#define FORWARD_PIN 15 
+#define FORWARD_BUTTON 6
+#define ONBOARD_LED 25   
+#define PWM_PIN 16
+#define PIN_15 15
+
+constexpr int sleep_time = 2000;
 constexpr float MAX_POWER = 1.0f;
 constexpr float MIN_POWER = 0.0f;
+constexpr uint PWM_FREQ = 10000;
+constexpr uint PWM_RANGE = 255;
 
 class DCMotor 
 {
@@ -21,16 +28,23 @@ public:
     void Shutdown();
     void OnUpdate();
 
-    void SetLED(uint pin, int val);
     void ToggleLED(uint pin);
     // Float 'p' to adjust power delivery, within range 0..1
     void Forward(float p);
     void Backward(float p);
     void Neutral();
     bool IsButtonPressed(uint button);
+    void UpdateDutyCycle(bool forward);
+    void SetDutyCycle(int cycle);
+    int GetDutyCycle() const { return m_DutyCycle; }
+
+private:
+    void InitPWM(uint pin, float pwm_range);
 
 private:
     bool m_Forward = false;
+    int m_DutyCycle = 0.0f;
+    uint m_SliceNum;
 };
 
 
